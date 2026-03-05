ba2c022adfdb113ce085c9bb167b5d07117df8e9
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-03-05 12:03:51 +0000

    Small improvements

--- inst/include/cpp20/internal/r_seq.h
+++ inst/include/cpp20/internal/r_seq.h
@@ -8,9 +8,9 @@
 namespace cpp20 {
 
 template <RNumericType T, RNumericType U>
-r_vec<r_sexp> sequences(r_vec<r_int> size, r_vec<T> from, r_vec<U> by){
+r_vec<r_sexp> sequences(const r_vec<r_int>& size, const r_vec<T>& from, const r_vec<U>& by){
 
-    using common_t = common_math_t<T, U>;
+    using common_t = common_r_t<T, U>;
     using commont_cpp_t = unwrap_t<common_t>;
 
     int size_n = size.length();
@@ -21,15 +21,15 @@ r_vec<r_sexp> sequences(r_vec<r_int> size, r_vec<T> from, r_vec<U> by){
         abort("from and by must both have length > 0");
     }
     
-    double out_size = sum(size, /*na_rm=*/ false);
-    double min_size = min(size, /*na_rm=*/ false);
+    r_int min_size = min(size, /*na_rm=*/ false);
 
-    if (is_na(out_size)){
+    if (is_na(min_size)){
         abort("size must not contain NA values");
     }
     if (min_size < 0){
         abort("size must be a vector of non-negative integers");
     }
+    
     r_size_t interrupt_counter = 0;
     
     r_vec<r_sexp> out(size_n);
