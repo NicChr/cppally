bd23d58da36715de3c9da6c663c3df0dea6d72d6
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-03-23 09:32:52 +0000

    Allow match to return 64-bit locations

--- inst/include/cpp20/r_match.h
+++ inst/include/cpp20/r_match.h
@@ -6,19 +6,23 @@
 namespace cpp20 {
 
 // 1-indexed match locations
-template <RVal T>
-r_vec<r_int> match(const r_vec<T>& needles, const r_vec<T>& haystack) {
+template <RVal T, typename U = r_int>
+requires (is<U, r_int> || is<U, r_int64>)
+r_vec<U> match(const r_vec<T>& needles, const r_vec<T>& haystack) {
 
   r_size_t n_needles = needles.length();
   r_size_t n_haystack = haystack.length();
 
-  if (n_haystack > r_limits<r_int>::max()){
-    abort("Cannot match to a long vector, please use a short vector");
+  if constexpr (is<U, r_int>){
+    if (n_haystack > r_limits<r_int>::max()){
+      abort("Cannot match to a long vector, please use match<T, r_int64> instead");
+    }
   }
 
   using key_t = unwrap_t<T>;
+  using int_t = unwrap_t<U>;
 
-  r_vec<r_int> out(n_needles);
+  r_vec<U> out(n_needles);
 
   if (n_needles == 0){
     return out;
@@ -32,7 +36,7 @@ r_vec<r_int> match(const r_vec<T>& needles, const r_vec<T>& haystack) {
         return out;
       }
     }
-    out.set(0, na<T>());
+    out.set(0, na<U>());
     return out;
   }
 
@@ -41,7 +45,7 @@ r_vec<r_int> match(const r_vec<T>& needles, const r_vec<T>& haystack) {
   auto* RESTRICT p_out = out.data();
 
   // Integer optimization: use table lookup for small integer ranges
-  if constexpr (is<key_t, int>) {
+  if constexpr (is<U, r_int> && is<key_t, int>) {
     r_vec<T> rng = range(haystack, /*na_rm=*/true);
     
     int min_val = unwrap(rng.get(0));
@@ -95,17 +99,17 @@ r_vec<r_int> match(const r_vec<T>& needles, const r_vec<T>& haystack) {
   }
 
   // Build hash table
-  ankerl::unordered_dense::map<key_t, int, internal::r_hash<T>, internal::r_hash_eq<T>> lookup;
+  ankerl::unordered_dense::map<key_t, int_t, internal::r_hash<T>, internal::r_hash_eq<T>> lookup;
   lookup.reserve(internal::get_hash_map_reserve_size<T>(n_haystack));
 
   for (r_size_t i = 0; i < n_haystack; ++i) {
-    lookup.try_emplace(p_haystack[i], static_cast<int>(i) + 1);
+    lookup.try_emplace(p_haystack[i], int_t(i) + int_t(1));
   }
 
   // Match needles
   for (r_size_t i = 0; i < n_needles; ++i) {
     auto it = lookup.find(p_needles[i]);
-    p_out[i] = (it != lookup.end() ? it->second : unwrap(na<r_int>()));
+    p_out[i] = (it != lookup.end() ? it->second : unwrap(na<U>()));
   }
 
   return out;
