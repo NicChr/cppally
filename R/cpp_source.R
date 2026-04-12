256985a15c609468d0cd0dfd7aa1849dd4537a58
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-04-12 12:47:49 +0100

    Exported cpp_source and cpp_eval

--- R/cpp_source.R
+++ R/cpp_source.R
@@ -46,8 +46,38 @@ generate_include_paths <- function(packages){
 }
 
 #' Compile C++20 code
-
-
+#'
+#' @description
+#' cpp11-style helpers to compile cpp20 code outside of a cpp20-linked package
+#' `cpp_source()` compiles and loads a single C++ for use in R,
+#' either from an expression or a cpp file. `cpp_eval()` evaluates a single C++
+#' expressions and returns the result. `void` return is not supported in `cpp_eval()`.
+#'
+#' @returns
+#' `cpp_source()` invisibly registers
+#' the `[[cpp20::register]]` tagged functions to R. \cr
+#' `cpp_eval()` returns the result of the evaluated C++ expression.
+#'
+#' @examples
+#'
+#' library(cpp20)
+#'
+#' # Expression returning the integer 0
+#' cpp_eval("r_int(0)")
+#'
+#' cpp_source(code = '
+#'   #include <cpp20.hpp>
+#'   using namespace cpp20;
+#'
+#'   [[cpp20::register]]
+#'   r_vec<r_str> unique_strs(r_vec<r_str> x){
+#'     return unique(x);
+#'   }
+#' ')
+#' x <- sample(letters, 10^3, TRUE)
+#' unique_strs(x)
+#' @rdname cpp_source
+#' @export
 cpp_source <- function(file, code = NULL, env = parent.frame(), clean = TRUE,
           quiet = TRUE, cxx_std = Sys.getenv("CXX_STD", "CXX20"), dir = tempfile()){
   stop_unless_installed(
@@ -118,7 +148,8 @@ cpp_source <- function(file, code = NULL, env = parent.frame(), clean = TRUE,
   source(r_path, local = env)
   dyn.load(shared_lib, local = TRUE, now = TRUE)
 }
-
+#' @rdname cpp_source
+#' @export
 cpp_eval <- function(code, env = parent.frame(), clean = TRUE, quiet = TRUE,
           cxx_std = Sys.getenv("CXX_STD", "CXX20")){
   cpp_source(
