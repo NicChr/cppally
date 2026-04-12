f1599fce94a65411fffdc836023aaea37483aebd
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-04-12 12:26:46 +0100

    Added cpp_eval

--- R/cpp_source.R
+++ R/cpp_source.R
@@ -118,3 +118,19 @@ cpp_source <- function(file, code = NULL, env = parent.frame(), clean = TRUE,
   source(r_path, local = env)
   dyn.load(shared_lib, local = TRUE, now = TRUE)
 }
+
+cpp_eval <- function(code, env = parent.frame(), clean = TRUE, quiet = TRUE,
+          cxx_std = Sys.getenv("CXX_STD", "CXX20")){
+  cpp_source(
+    code = paste(c(
+      "#include <cpp20/r_dispatch.h>",
+      "#include <cpp20.hpp>",
+      "using namespace cpp20;",
+      "using internal::cpp_to_sexp;",
+      "[[cpp20::register]]",
+      paste0("SEXP f() { return cpp_to_sexp(", code, "); }")
+    ), collapse = "\n"),
+    env = env, clean = clean, quiet = quiet, cxx_std = cxx_std
+  )
+  f()
+}
