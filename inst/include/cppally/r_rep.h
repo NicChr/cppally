78dd8f79d96e94596a975a0917d387b909a4020e
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-05-14 15:34:31 +0100

    New rep helpers

--- inst/include/cppally/r_rep.h
+++ inst/include/cppally/r_rep.h
@@ -2,6 +2,7 @@
 #define CPPALLY_R_REP_H
 
 #include <cppally/r_vec.h>
+#include <cppally/sugar/r_stats.h>
 #include <cppally/r_visit.h>
 #include <cppally/r_copy.h>
 
@@ -66,6 +67,45 @@ inline r_df resize(const r_df& x, r_size_t n){
 
 inline r_sexp resize(const r_sexp& x, r_size_t n){
     return r_sexp(CPPALLY_VIEW_AND_APPLY(x, /*return_type = */ SEXP, /*fn = */ resize, n));
+}
+
+template <RVector T>
+T rep(const T& x, const r_vec<r_int>& times){
+    
+    r_size_t n = length(x);
+
+    r_size_t out_size;
+    r_size_t n_times = length(times);
+    
+    if (n_times == 1){
+        out_size = n * times.data()[0];
+        return rep_len(x, out_size);
+    } else if (n_times == n){
+        r_dbl out_size = sum(times, false);
+        if (is_na(out_size)){
+            abort("%s: `times` contains `NA` values", __func__);
+        }
+        T out(out_size);
+        r_size_t k = 0;
+        for (r_size_t i = 0; i < n; ++i){
+          out.fill(k, times.data()[i], x.view(i));
+          k += times.data()[i];
+        }
+        return out;
+    } else {
+        abort("%s: `length(times)` must be 1 or match `length(x)`", __func__);
+    }
+}
+
+template <RVector T>
+T rep_each(const T& x, const r_vec<r_int>& each){
+  if (length(each) == 1){
+    if (identical(each.get(0), r_int(1))){
+      return x;
+    }
+    return rep(x, rep_len(each, length(x)));
+  }
+  return rep(x, each);
 }
 
 }
