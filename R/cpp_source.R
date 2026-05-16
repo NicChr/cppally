aa1a5a37a0b8680b85ef3f61ef6df2767a34228e
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-05-16 15:44:43 +0100

    Debugging macOS errors

--- R/cpp_source.R
+++ R/cpp_source.R
@@ -265,7 +265,7 @@ cpp_source <- function(file, code = NULL, env = parent.frame(),
                            tools::file_path_sans_ext(orig_file_path), error_messages,
                            fixed = TRUE)
     cat(error_messages)
-    stop("Compilation failed.", call. = FALSE)
+    stop("Compilation failed:\n", paste(error_messages, collapse = "\n"), call. = FALSE)
   }
   shared_lib <- file.path(dir, "src", paste0(tools::file_path_sans_ext(new_file_name),
                                              .Platform$dynlib.ext))
