10abf3c53644d6da1738ec435d10c59f55e1f630
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-06-18 12:04:31 +0100

    Improvements to omp simd helpers

--- inst/include/cppally/r_omp.h
+++ inst/include/cppally/r_omp.h
@@ -9,28 +9,103 @@ namespace omp {
 
 // OMP helpers
 
-// Apply a function to *this across range of target
+// Apply unary function to source's elements and set target's elements to output (via OMP SIMD)
 template <RVectorisable T, RVectorisable U>
-void simd_apply(r_vec<T>& source, r_vec<U>& target, auto f) {
+void simd_apply(const r_vec<T>& source, r_vec<U>& target, auto f) {
+    
     r_size_t n = source.length();
-    if (n != target.length()) [[unlikely]] {
-        abort("`length(source)` must equal `length(target)`");
+
+    if (n == 0){
+        return;
+    }
+
+    if (n != target.length() && n != 1) [[unlikely]] {
+        abort("`length(source)` must equal `length(target)` or 1");
     }
     const unwrap_t<T>* p_source = source.data();
     unwrap_t<U>* p_target = target.data();
     int n_threads = internal::calc_threads(n);
-    if (n_threads > 1){
-        OMP_PARALLEL_FOR_SIMD(n_threads)
-        for (r_size_t i = 0; i < n; ++i) p_target[i] = unwrap(f(T(p_source[i])));
+    if (n == 1){
+        T source_val = T{p_source[0]};
+        if (n_threads > 1){
+            OMP_PARALLEL_FOR_SIMD(n_threads)
+            for (r_size_t i = 0; i < n; ++i) p_target[i] = unwrap(f(source_val));
+        } else {
+            OMP_SIMD
+            for (r_size_t i = 0; i < n; ++i) p_target[i] = unwrap(f(source_val));
+        }
     } else {
-        OMP_SIMD
-        for (r_size_t i = 0; i < n; ++i) p_target[i] = unwrap(f(T(p_source[i])));
+        if (n_threads > 1){
+            OMP_PARALLEL_FOR_SIMD(n_threads)
+            for (r_size_t i = 0; i < n; ++i) p_target[i] = unwrap(f(T(p_source[i])));
+        } else {
+            OMP_SIMD
+            for (r_size_t i = 0; i < n; ++i) p_target[i] = unwrap(f(T(p_source[i])));
+        }
     }
 }
 
+// apply unary function directly to target (via OMP SIMD)
 template <RVectorisable T>
-void simd_apply(r_vec<T>& x, auto f) {
-    simd_apply(x, x, f);
+void simd_apply(r_vec<T>& target, auto f) {
+    simd_apply(target, target, f);
+}
+
+// Apply binary function (via OMP SIMD)
+template <RVectorisable T, RVectorisable U, RVectorisable V>
+void simd_apply(const r_vec<T>& lhs, const r_vec<U>& rhs, r_vec<V>& target, auto f) {
+    
+    r_size_t n = target.length();
+
+    if (n == 0){
+        return;
+    }
+    
+    const unwrap_t<T>* p_lhs = lhs.data();
+    const unwrap_t<U>* p_rhs = rhs.data();
+    unwrap_t<V>* p_target = target.data();
+
+    int n_threads = internal::calc_threads(n);
+
+    if (lhs.length() == 1 && rhs.length() == 1){
+        T lhs_v = T(p_lhs[0]);
+        U rhs_v = U(p_rhs[0]);
+        if (n_threads > 1){
+            OMP_PARALLEL_FOR_SIMD(n_threads)
+            for (r_size_t i = 0; i < n; ++i) p_target[i] = unwrap(f(lhs_v, rhs_v));
+        } else {
+            OMP_SIMD
+            for (r_size_t i = 0; i < n; ++i) p_target[i] = unwrap(f(lhs_v, rhs_v));
+        }
+    } else if (lhs.length() == 1 && rhs.length() == n){
+        T lhs_v = T(p_lhs[0]);
+        if (n_threads > 1){
+            OMP_PARALLEL_FOR_SIMD(n_threads)
+            for (r_size_t i = 0; i < n; ++i) p_target[i] = unwrap(f(lhs_v, U(p_rhs[i])));
+        } else {
+            OMP_SIMD
+            for (r_size_t i = 0; i < n; ++i) p_target[i] = unwrap(f(lhs_v, U(p_rhs[i])));
+        }
+    } else if (rhs.length() == 1 && lhs.length() == n){
+        U rhs_v = U(p_rhs[0]);
+        if (n_threads > 1){
+            OMP_PARALLEL_FOR_SIMD(n_threads)
+            for (r_size_t i = 0; i < n; ++i) p_target[i] = unwrap(f(T(p_lhs[i]), rhs_v));
+        } else {
+            OMP_SIMD
+            for (r_size_t i = 0; i < n; ++i) p_target[i] = unwrap(f(T(p_lhs[i]), rhs_v));
+        }
+    } else if (lhs.length() == n && rhs.length() == n){
+        if (n_threads > 1){
+            OMP_PARALLEL_FOR_SIMD(n_threads)
+            for (r_size_t i = 0; i < n; ++i) p_target[i] = unwrap(f(T(p_lhs[i]), U(p_rhs[i])));
+        } else {
+            OMP_SIMD
+            for (r_size_t i = 0; i < n; ++i) p_target[i] = unwrap(f(T(p_lhs[i]), U(p_rhs[i])));
+        }
+    } else {
+        abort("`length(lhs)` or `length(rhs)` must be 1 or both must equal `length(target)`");
+    }
 }
 
 template <RVectorisable T, typename Acc>
