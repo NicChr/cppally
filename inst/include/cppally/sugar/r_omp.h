a47dfd565b842285b41248a862e3253610a06071
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-06-18 07:26:51 +0100

    Small safety improvements

--- inst/include/cppally/sugar/r_omp.h
+++ inst/include/cppally/sugar/r_omp.h
@@ -10,14 +10,14 @@ namespace omp {
 // OMP helpers
 
 // Apply a function to *this across range of target
-template <RVectorisable T, RVectorisable U, typename F>
-void simd_apply(r_vec<T>& source, r_vec<U>& target, F f) {
+template <RVectorisable T, RVectorisable U>
+void simd_apply(r_vec<T>& source, r_vec<U>& target, auto f) {
     r_size_t n = source.length();
     if (n != target.length()) [[unlikely]] {
         abort("`length(source)` must equal `length(target)`");
     }
-    const unwrap_t<T>* RESTRICT p_source = source.data();
-    unwrap_t<U>* RESTRICT p_target = target.data();
+    const unwrap_t<T>* p_source = source.data();
+    unwrap_t<U>* p_target = target.data();
     int n_threads = internal::calc_threads(n);
     if (n_threads > 1){
         OMP_PARALLEL_FOR_SIMD(n_threads)
@@ -28,8 +28,13 @@ void simd_apply(r_vec<T>& source, r_vec<U>& target, F f) {
     }
 }
 
-template <RVectorisable T, typename Acc, typename F>
-void simd_reduce_add(const r_vec<T>& x, Acc& init, F f) noexcept {
+template <RVectorisable T>
+void simd_apply(r_vec<T>& x, auto f) {
+    simd_apply(x, x, f);
+}
+
+template <RVectorisable T, typename Acc>
+void simd_reduce_add(const r_vec<T>& x, Acc& init, auto f) noexcept {
     r_size_t n = x.length();
     const unwrap_t<T>* RESTRICT p_x = x.data();
     int n_threads = internal::calc_threads(n);
