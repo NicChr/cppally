7a79ca00676b27d7b0e85ff50a7043d4229e9229
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-06-24 09:05:20 +0100

    Small tweaks

--- inst/include/cppally/r_pmap.h
+++ inst/include/cppally/r_pmap.h
@@ -11,7 +11,7 @@ namespace cppally {
 // simd applies the loop in an OMP SIMD region; only honoured when every type is RVectorisable,
 // otherwise it falls through to the scalar path.
 template <bool simd = false, bool parallel = false, typename F, RVal... Ts>
-  requires std::invocable<F, r_size_t, Ts...>
+  requires std::invocable<F&, r_size_t, Ts...>
 auto pmap_impl(F fn, const r_vec<Ts>&... vecs) {
 
   #define CPPALLY_DO_MAP_WITH_DATA for (r_size_t i = 0; i < n; ++i) p_out[i] = unwrap(fn(i, Ts(ps[i])...));
@@ -19,7 +19,7 @@ auto pmap_impl(F fn, const r_vec<Ts>&... vecs) {
 
   constexpr int n_vecs = sizeof...(Ts);
 
-  using out_t = std::invoke_result_t<F, r_size_t, Ts...>;
+  using out_t = std::invoke_result_t<F&, r_size_t, Ts...>;
   static_assert(RVal<out_t>, "pmap: output type is not storable in r_vec");
 
   if constexpr (n_vecs == 0) {
@@ -97,32 +97,32 @@ auto pmap_impl(F fn, const r_vec<Ts>&... vecs) {
 
 // map m x n vectors to 1 x n output by applying a user function: fn(x0, x1, x2, ..., xn)
 template <typename F, RVal... Ts>
-  requires std::invocable<F, Ts...>
+  requires std::invocable<F&, Ts...>
 auto pmap(F fn, const r_vec<Ts>&... vecs) {
   return pmap_impl<false>([&](r_size_t, Ts... vs){ return fn(vs...); }, vecs...);
 }
 
 // map m x n vectors to 1 x n output by applying a user function: fn(r_size_t index, x0, x1, x2, ..., xn)
 template <typename F, RVal... Ts>
-  requires std::invocable<F, r_size_t, Ts...>
+  requires std::invocable<F&, r_size_t, Ts...>
 auto pmap_with_index(F fn, const r_vec<Ts>&... vecs) {
   return pmap_impl<false>(fn, vecs...);
 }
 
 template <typename F, RVal... Ts>
-  requires std::invocable<F, Ts...>
+  requires std::invocable<F&, Ts...>
 auto pmap_simd(F fn, const r_vec<Ts>&... vecs) {
   return pmap_impl<true>([&](r_size_t, Ts... vs){ return fn(vs...); }, vecs...);
 }
 
 template <typename F, RVal... Ts>
-  requires std::invocable<F, Ts...>
+  requires std::invocable<F&, Ts...>
 auto pmap_parallel(F fn, const r_vec<Ts>&... vecs) {
   return pmap_impl<false, true>([&](r_size_t, Ts... vs){ return fn(vs...); }, vecs...);
 }
 
 template <typename F, RVal... Ts>
-  requires std::invocable<F, Ts...>
+  requires std::invocable<F&, Ts...>
 auto pmap_parallel_simd(F fn, const r_vec<Ts>&... vecs) {
   return pmap_impl<true, true>([&](r_size_t, Ts... vs){ return fn(vs...); }, vecs...);
 }
