4e59054272960da1fe3d7c57e9e253cb273fd2f1
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-06-28 16:36:32 +0100

    Removed sequences and added new helpers

--- inst/include/cppally/sugar/r_seq.h
+++ inst/include/cppally/sugar/r_seq.h
@@ -3,81 +3,102 @@
 
 #include <cppally/r_utils.h>
 #include <cppally/r_vec.h>
-#include <cppally/sugar/r_stats.h>
+#include <cppally/sugar/r_math.h>
 #include <cppally/r_coerce.h>
-#include <vector>
 
 namespace cppally {
 
-template <RNumericType T, RNumericType U>
-std::vector<common_r_t<r_vec<T>, r_vec<U>>> sequences(const r_vec<r_int>& size, const r_vec<T>& from, const r_vec<U>& by){
+namespace internal {
+template <typename T>
+concept RPlainNumber = any<T, r_int, r_int64, r_dbl>;
+}
 
-    using common_t = common_r_t<T, U>;
-    using commont_cpp_t = unwrap_t<common_t>;
+template <internal::RPlainNumber T, internal::RPlainNumber U>
+r_vec<common_r_t<T, U>> sequence(r_int size, T from, U by){
 
-    int size_n = size.length();
-    int from_n = from.length();
-    int by_n = by.length();
+    using common_t   = common_r_t<T, U>;
+    using common_cpp = unwrap_t<common_t>;
 
-    if (size_n > 0 && (from_n <= 0 || by_n <= 0)){
-        abort("from and by must both have length > 0");
+    if (is_na(size)){
+        abort("size must not be NA");
     }
-    
-    r_int min_size = range(size, /*na_rm=*/ false).get(0);
+    if ( (size < 0).is_true() ){
+        abort("size must be non-negative");
+    }
 
-    if (is_na(min_size)){
-        abort("size must not contain NA values");
+    if (is_na(from)){
+        abort("from contains NA values");
     }
-    if (min_size < 0){
-        abort("size must be a vector of non-negative integers");
+    if (is_na(by)){
+        abort("by contains NA values");
     }
-    
+
+    auto start     = unwrap(from);
+    auto increment = unwrap(by);
+
+    auto seq_size = unwrap(size);
+    auto out = r_vec<common_t>(seq_size);
+
+    common_cpp current_val = start;
     r_size_t interrupt_counter = 0;
-    
-    std::vector<r_vec<common_t>> out;
-    out.reserve(size_n);
 
-    if (size_n > 0){
-        
-        for (int i = 0, bi = 0, fi = 0; i < size_n;
-        recycle_index(bi, by_n),
-        recycle_index(fi, from_n),
-        ++i){
-            auto seq_size = unwrap(size.get(i));
-            auto curr_seq = r_vec<common_t>(seq_size);
-            auto start = unwrap(from.get(fi));
-            auto increment = unwrap(by.get(bi));
-            if (is_na(start)){
-                abort("from contains NA values");
-            }
-            if (is_na(increment)){
-                abort("by contains NA values");
-            }
-
-            commont_cpp_t current_val = start;
-
-            for (int j = 0; j < seq_size; ++j, ++interrupt_counter){
-                if (interrupt_counter == 100000000){
-                    check_user_interrupt();
-                    interrupt_counter = 0;
-                }
-                if constexpr (RIntegerType<common_t>){
-                    curr_seq.set(j, common_t(current_val));
-
-                    // Only add if we need the next value
-                    if (j < seq_size - 1) {
-                        if (__builtin_add_overflow(current_val, increment, &current_val)) [[unlikely]] {
-                            abort("Integer overflow in sequence %d, please use doubles", i + 1);
-                        }
-                    }
-                } else {
-                    curr_seq.set(j, common_t( start + (j * increment) ));
+    for (r_size_t j = 0; j < seq_size; ++j, ++interrupt_counter){
+        if (interrupt_counter == 100000000) [[unlikely]] {
+            check_user_interrupt();
+            interrupt_counter = 0;
+        }
+        if constexpr (RIntegerType<common_t>){
+            out.set(j, common_t(current_val));
+            if (j < seq_size - 1){
+                if (__builtin_add_overflow(current_val, increment, &current_val)) [[unlikely]] {
+                    abort("Integer overflow in sequence, please use doubles");
                 }
             }
-            out.push_back(curr_seq);
+        } else {
+            out.set(j, common_t( start + (j * increment) ));
         }
     }
     return out;
+}
+
+// size of the sequence from..to stepping by `by`
+template <internal::RPlainNumber T, internal::RPlainNumber U, internal::RPlainNumber V>
+r_int seq_size(T from, U to, V by){
+    auto del = to - from;
+    // from == to with a zero increment is a well-defined length-1 sequence
+    r_dbl ratio = ( (del == 0).is_true() && (by == 0).is_true() ) ? r_dbl(0.0) : del / by;
+    if ( (ratio < 0).is_true() ){
+        abort("sequence length is negative, please check the sign of `by`");
+    }
+    // + 1e-10 absorbs floating point error before truncating
+    return as<r_int>( trunc(ratio + r_dbl(1e-10)) + r_dbl(1.0) );
+}
+
+// first value given a size, an end point and an increment
+template <internal::RPlainNumber T, internal::RPlainNumber U>
+auto seq_start(r_int size, T to, U by){
+    return to - max(size - r_int(1), r_int(0)) * by;
+}
+
+// last value given a size, a start point and an increment
+template <internal::RPlainNumber T, internal::RPlainNumber U>
+auto seq_end(r_int size, T from, U by){
+    return from + max(size - r_int(1), r_int(0)) * by;
+}
+
+// increment given a size and the two end points
+template <internal::RPlainNumber T, internal::RPlainNumber U>
+r_dbl seq_increment(r_int size, T from, U to){
+    if ( (from == to).is_true() || (size == r_int(1)).is_true() ){
+        return r_dbl(0.0);
+    }
+    return (to - from) / max(size - r_int(1), r_int(0));
+}
+
+// build the sequence from..to stepping by `by`
+template <internal::RPlainNumber T, internal::RPlainNumber U, internal::RPlainNumber V>
+r_vec<common_r_t<T, V>> seq(T from, U to, V by){
+    return sequence(seq_size(from, to, by), from, by);
 }
 
 }
