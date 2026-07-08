87ba24e7ca20db37b637e52a6998c6e7061769e2
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-07-08 07:58:50 +0100

    Use RNumber concept

--- inst/include/cppally/sugar/r_seq.h
+++ inst/include/cppally/sugar/r_seq.h
@@ -8,12 +8,7 @@
 
 namespace cppally {
 
-namespace internal {
-template <typename T>
-concept RPlainNumber = RShortIntegerType<T> || RLongIntegerType<T> || RFloatType<T>;
-}
-
-template <internal::RPlainNumber T, internal::RPlainNumber U>
+template <RNumber T, RNumber U>
 auto sequence(int size, T from, U by){
 
     using common_t   = common_r_t<T, U>;
@@ -59,7 +54,7 @@ auto sequence(int size, T from, U by){
 }
 
 // size of the sequence from..to stepping by `by`
-template <internal::RPlainNumber T, internal::RPlainNumber U, internal::RPlainNumber V>
+template <RNumber T, RNumber U, RNumber V>
 int seq_size(T from, U to, V by){
     auto del = to - from;
     // from == to with a zero increment is a well-defined length-1 sequence
@@ -76,19 +71,19 @@ int seq_size(T from, U to, V by){
 }
 
 // first value given a size, an end point and an increment
-template <internal::RPlainNumber T, internal::RPlainNumber U>
+template <RNumber T, RNumber U>
 auto seq_start(int size, T to, U by){
     return to - max(size - r_int(1), r_int(0)) * by;
 }
 
 // last value given a size, a start point and an increment
-template <internal::RPlainNumber T, internal::RPlainNumber U>
+template <RNumber T, RNumber U>
 auto seq_end(int size, T from, U by){
     return from + max(size - r_int(1), r_int(0)) * by;
 }
 
 // increment given a size and the two end points
-template <internal::RPlainNumber T, internal::RPlainNumber U>
+template <RNumber T, RNumber U>
 r_dbl seq_increment(int size, T from, U to){
     if ( (from == to).is_true() || (size == 1) ){
         return r_dbl(0.0);
@@ -98,7 +93,7 @@ r_dbl seq_increment(int size, T from, U to){
 
 // build the sequence from..to stepping by `by`
 // `to` only sets the length; widen from/by to its type so the right vector is built directly
-template <internal::RPlainNumber T, internal::RPlainNumber U, internal::RPlainNumber V>
+template <RNumber T, RNumber U, RNumber V>
 auto seq(T from, U to, V by){
     using out_t = common_r_t<T, U, V>;
     return sequence(seq_size(from, to, by), as<out_t>(from), as<out_t>(by));
