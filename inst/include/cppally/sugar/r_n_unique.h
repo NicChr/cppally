35befde0f07ea47f67b47762db4ab3fcaffc8ac1
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-07-14 13:36:17 +0100

    Small improvement to n_unique

--- inst/include/cppally/sugar/r_n_unique.h
+++ inst/include/cppally/sugar/r_n_unique.h
@@ -34,17 +34,20 @@ inline r_size_t n_unique(const T& x) {
     if (done) return n_unq;
   }
 
-  // Hash set for O(n) de-duplication
-  ankerl::unordered_dense::set<
-    unwrap_t<data_t>, 
-    internal::r_hash<data_t>, 
+  // Hash map for O(n) de-duplication
+  // A map with a discarded int payload instead of a set: it shares its
+  // instantiation with the maps in make_groups()/match()
+  ankerl::unordered_dense::map<
+    unwrap_t<data_t>,
+    int,
+    internal::r_hash<data_t>,
     internal::r_hash_eq<data_t>
   > seen;
 
   seen.reserve(internal::get_hash_map_reserve_size<data_t>(n));
 
   for (r_size_t i = 0; i < n; ++i) {
-    seen.insert(p_x[i]);
+    seen.try_emplace(p_x[i], 0);
     // Since r_lgl can be either true, false or NA, we can safely return early if n_unique == 3
     if constexpr (is<data_t, r_lgl>){
       if (seen.size() == 3) return 3;
