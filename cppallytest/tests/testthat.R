c8b9902f0fedfa3edd1aa2c9520593ccd8e44796
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-07-18 13:46:59 +0100

    Conditionally run tests

--- cppallytest/tests/testthat.R
+++ cppallytest/tests/testthat.R
@@ -6,7 +6,9 @@
 # * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
 # * https://testthat.r-lib.org/articles/special-files.html
 
-library(testthat)
-library(cppallytest)
 
-test_check("cppallytest")
+if (requireNamespace("testthat", quietly = TRUE)) {
+  library(testthat)
+  library(cppallytest)
+  test_check("cppallytest")
+}
