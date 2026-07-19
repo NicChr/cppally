7625c4f02c0d102416412f9e1af800b6b8aa9684
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-07-19 09:33:37 +0100

    Renamed macro to CPPALLY_MAKE_VISITOR

--- inst/include/cppally/r_visit.h
+++ inst/include/cppally/r_visit.h
@@ -193,17 +193,7 @@ inline bool is_visitable(const r_sexp& x){
 // Builds a visitor (for r_sexp_visit) that applies the named
 // overload set `fn` with the given bound args, converting each arm's result to `ret`.
 // The requires-clause excludes unvisitable r_sexp types
-// define CPPALLY_VISITOR(ret, fn, ...)
-//     [&]<typename visitor_v_t> requires (!is<visitor_v_t, r_sexp>)                         
-//         (const visitor_v_t& v_)                                                           
-//         -> decltype(static_cast<ret>(fn(v_ __VA_OPT__(,) __VA_ARGS__)))                   
-//     { return static_cast<ret>(fn(v_ __VA_OPT__(,) __VA_ARGS__)); }
-
-#ifdef MAKE_VISITOR
-#warning "MAKE_VISITOR pragma already defined and will be overwritten!"
-#endif
-
-#define MAKE_VISITOR(ret, v, ...)                                               \
+#define CPPALLY_MAKE_VISITOR(ret, v, ...)                                       \
     [&]<typename visitor_v_t> requires (!is<visitor_v_t, r_sexp>)               \
         (const visitor_v_t& v)                                                  \
         -> decltype(static_cast<ret>(__VA_ARGS__))                              \
