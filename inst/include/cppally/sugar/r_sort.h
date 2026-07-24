e030e2247a498245b69f76fa1b933f458dfc1fae
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-07-24 17:51:33 +0100

    Code simplifications to r_sort

--- inst/include/cppally/sugar/r_sort.h
+++ inst/include/cppally/sugar/r_sort.h
@@ -20,67 +20,26 @@ namespace internal {
 // NAs are ordered last
 // Internal function to be used for low overhead sorting small vectors (n<500)
 template <RSortableVector T>
-r_vec<r_int> cpp_order(const T& x) {
-    using data_t = typename T::data_type;
+r_vec<r_int> cpp_order(const T& x, bool stable = true) {
     int n = x.length();
-    r_vec<r_int> p(n);
-    p.iota();
+    r_vec<r_int> pv(n);
+    pv.iota();
+    auto* RESTRICT p = pv.data();
 
-    auto *p_x = x.data();
-
-    if constexpr (RNumericType<data_t>){
-        std::sort(p.data(), p.data() + n, [&](int i, int j) {
+    if (stable){
+        std::stable_sort(p, p + n, [&](int i, int j) noexcept {
             if (is_na(x.view(i))) return false;
             if (is_na(x.view(j))) return true;
-            return p_x[i] < p_x[j];
+            return (x.view(i) < x.view(j)).is_true();
         });
     } else {
-        // Below works on strings
-        std::sort(p.data(), p.data() + n, [&](int i, int j) {
-            auto res = x.view(i) < x.view(j);
-            if (is_na(res)){
-                if (is_na(x.view(i))){
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-            return static_cast<bool>(unwrap(res));
-        });
-    }
-    return p;
-}
-
-template <RSortableVector T>
-r_vec<r_int> cpp_stable_order(const T& x) {
-    using data_t = typename T::data_type;
-    int n = x.length();
-    r_vec<r_int> p(n);
-    p.iota();
-
-    auto *p_x = x.data();
-
-    if constexpr (RNumericType<data_t>){
-        std::stable_sort(p.data(), p.data() + n, [&](int i, int j) {
+        std::sort(p, p + n, [&](int i, int j) noexcept {
             if (is_na(x.view(i))) return false;
             if (is_na(x.view(j))) return true;
-            return p_x[i] < p_x[j];
-        });
-    } else {
-        // Below works on strings
-        std::stable_sort(p.data(), p.data() + n, [&](int i, int j) {
-            auto res = x.view(i) < x.view(j);
-            if (is_na(res)){
-                if (is_na(x.view(i))){
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-            return static_cast<bool>(unwrap(res));
+            return (x.view(i) < x.view(j)).is_true();
         });
     }
-    return p;
+    return pv;
 }
 
 }
@@ -94,12 +53,8 @@ inline r_vec<r_int> order(const T& x, bool preserve_ties = true) {
     using base_t = unwrap_t<data_t>;
 
     uint32_t n = x.length();
-    if (n < 500){
-        if (preserve_ties){
-            return internal::cpp_stable_order(x);
-        } else {
-            return internal::cpp_order(x);
-        }
+    if (n < 1000){
+        return internal::cpp_order(x, preserve_ties);
     }
 
     if constexpr (RNumericType<data_t>) {
@@ -316,11 +271,7 @@ inline r_vec<r_int> order(const T& x, bool preserve_ties = true) {
         
         return out;
     } else {
-        if (preserve_ties){
-            return internal::cpp_stable_order(x);
-        } else {
-            return internal::cpp_order(x);
-        }
+        return internal::cpp_order(x, preserve_ties);
     }
 }
 
