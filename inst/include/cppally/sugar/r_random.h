abe538bc7c036717fdce1b38f2d4e7224f479f1c
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-07-27 17:40:08 +0100

    Remove unused helpers

--- inst/include/cppally/sugar/r_random.h
+++ inst/include/cppally/sugar/r_random.h
@@ -3,7 +3,6 @@
 
 #include <cppally/r_vec.h>
 #include <R_ext/Random.h>
-#include <ankerl/unordered_dense.h>
 #include <random>
 
 namespace cppally {
@@ -85,90 +84,100 @@ private:
     std::mt19937_64 engine_;
 };
 
-// Sample indices with replacement.
-template <typename index_t>
-requires (any<index_t, int, r_size_t>)
-auto sample_indices_with_replacement(index_t n, index_t size, random_stream& r) {
+// Out of scope for this header but you can implement R style vectorised sampling as shown below
 
-    using data_t = as_r_scalar_t<index_t>;
+// #include <ankerl/unordered_dense.h>
 
-    if (n <= 0) [[unlikely]] {
-        abort("population size must be > 0");
-    }
+// // Sample indices with replacement.
+// template <typename index_t>
+// requires (any<index_t, int, r_size_t>)
+// auto sample_indices_with_replacement(index_t n, index_t size, random_stream& r) {
 
-    if (size < 0) [[unlikely]] {
-        abort("sample size must be >= 0");
-    }
+//     using data_t = as_r_scalar_t<index_t>;
 
-    if (size == 0) {
-        return r_vec<data_t>();
-    }
+//     if (n <= 0) [[unlikely]] {
+//         abort("population size must be > 0");
+//     }
 
-    r_vec<data_t> out(size);
-    for (index_t i = 0; i < size; ++i){
-        out.set(i, data_t(r.index(0, n - 1)));
-    }
-    return out;
-}
-template <typename index_t>
-requires (any<index_t, int, r_size_t>)
-auto sample_indices_with_replacement(index_t n, index_t size) {
-    random_stream r;
-    return sample_indices_with_replacement(n, size, r);
-}
+//     if (size < 0) [[unlikely]] {
+//         abort("sample size must be >= 0");
+//     }
 
-// Sample indices without replacement.
-// Partial Fisher-Yates over an identity array that is never materialised.
-template <typename index_t>
-requires (any<index_t, int, r_size_t>)
-auto sample_indices_without_replacement(index_t n, index_t size, random_stream& r) {
+//     if (size == 0) {
+//         return r_vec<data_t>();
+//     }
 
-    using data_t = as_r_scalar_t<index_t>;
+//     r_vec<data_t> out(size);
+//     for (index_t i = 0; i < size; ++i){
+//         out.set(i, data_t(r.index(0, n - 1)));
+//     }
+//     return out;
+// }
+// template <typename index_t>
+// requires (any<index_t, int, r_size_t>)
+// auto sample_indices_with_replacement(index_t n, index_t size) {
+//     random_stream r;
+//     return sample_indices_with_replacement(n, size, r);
+// }
 
-    if (n <= 0) [[unlikely]] {
-        abort("population size must be > 0");
-    }
+// // Sample indices without replacement.
+// // Partial Fisher-Yates over an identity array that is never materialised.
+// template <typename index_t>
+// requires (any<index_t, int, r_size_t>)
+// auto sample_indices_without_replacement(index_t n, index_t size, random_stream& r) {
 
-    if (size < 0) [[unlikely]] {
-        abort("sample size must be >= 0");
-    }
+//     using data_t = as_r_scalar_t<index_t>;
 
-    if (size > n) [[unlikely]] {
-        abort("sample size must be <= population size when sampling without replacement");
-    }
+//     if (n <= 0) [[unlikely]] {
+//         abort("population size must be > 0");
+//     }
 
-    if (size == 0) {
-        return r_vec<data_t>();
-    }
+//     if (size < 0) [[unlikely]] {
+//         abort("sample size must be >= 0");
+//     }
 
-    // position -> value, only where that value is no longer the identity
-    ankerl::unordered_dense::map<index_t, index_t> swapped;
-    swapped.reserve(static_cast<std::size_t>(size));
+//     if (size > n) [[unlikely]] {
+//         abort("sample size must be <= population size when sampling without replacement");
+//     }
 
-    r_vec<data_t> out(size);
+//     if (size == 0) {
+//         return r_vec<data_t>();
+//     }
 
-    for (index_t i = 0; i < size; ++i){
-        index_t j = r.index(i, n - 1);
+//     // position -> value, only where that value is no longer the identity
+//     ankerl::unordered_dense::map<index_t, index_t> swapped;
+//     swapped.reserve(static_cast<std::size_t>(size));
 
-        auto it_j = swapped.find(j);
-        index_t val_j = it_j == swapped.end() ? j : it_j->second;
+//     r_vec<data_t> out(size);
 
-        auto it_i = swapped.find(i);
-        index_t val_i = it_i == swapped.end() ? i : it_i->second;
+//     for (index_t i = 0; i < size; ++i){
+//         index_t j = r.index(i, n - 1);
 
-        out.set(i, data_t(val_j));
-        // j is always drawn from [i, n), so position i is never read again
-        swapped[j] = val_i;
-    }
-    return out;
-}
+//         auto it_j = swapped.find(j);
+//         index_t val_j = it_j == swapped.end() ? j : it_j->second;
 
-template <typename index_t>
-requires (any<index_t, int, r_size_t>)
-auto sample_indices_without_replacement(index_t n, index_t size) {
-    random_stream r;
-    return sample_indices_without_replacement(n, size, r);
-}
+//         auto it_i = swapped.find(i);
+//         index_t val_i = it_i == swapped.end() ? i : it_i->second;
+
+//         out.set(i, data_t(val_j));
+//         // j is always drawn from [i, n), so position i is never read again
+//         swapped[j] = val_i;
+//     }
+//     return out;
+// }
+
+// template <typename index_t>
+// requires (any<index_t, int, r_size_t>)
+// auto sample_indices_without_replacement(index_t n, index_t size) {
+//     random_stream r;
+//     return sample_indices_without_replacement(n, size, r);
+// }
+
+// template <RAtomicVector T>
+// auto sample(const T& x, r_size_t size) {
+//     r_vec<r_int> indices = sample_indices_with_replacement<int>(x.length(), size);
+//     return pmap_parallel_simd([&x](r_int idx){ return x.view(static_cast<r_size_t>(unwrap(idx)));}, indices);
+// }
 
 }
 
