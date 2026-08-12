73123d6148b25c4ab584c776b4b67d4bb1bad1ab
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-08-12 10:45:13 +0100

    Move dense int map into groups/

--- inst/include/cppally/match/match.h
+++ inst/include/cppally/match/match.h
@@ -4,7 +4,7 @@
 #include <cppally/r_coerce.h>
 #include <cppally/hash/hash.h>
 #include <cppally/sugar/r_stats.h>
-#include <cppally/sugar/r_dense_int_map.h>
+#include <cppally/groups/dense_int_map.h>
 #include <cppally/r_pmap.h>
 #include <ankerl/unordered_dense.h> // Hash maps for group IDs + unique + match
 #include <vector>
