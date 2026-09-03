4ba6ae7bde7d5b717e9e4995ae279f4b11741d88
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-09-03 10:24:43 +0100

    Use abs2

--- inst/include/cppally/scalar/r_dbl.h
+++ inst/include/cppally/scalar/r_dbl.h
@@ -2,6 +2,7 @@
 #define CPPALLY_R_DBL_H
 
 #include <cppally/r_concepts.h>
+#include <cppally/utils.h>
 #include <limits>
 #include <bit> // for std::bit_cast
 
@@ -55,21 +56,15 @@ struct r_dbl {
   }
 
   constexpr bool is_infinite() const noexcept {
-    return constexpr_fabs(value) == static_cast<double>(inf());
+    return internal::abs2(value) == static_cast<double>(inf());
   }
 
   constexpr bool is_finite() const noexcept {
-    return constexpr_fabs(value) < static_cast<double>(inf());
+    return internal::abs2(value) < static_cast<double>(inf());
   }
 
   constexpr bool is_nan() const noexcept {
     return is_na() && !internal::has_na_real_payload(value);
-  }
-
-  private: 
-
-  static constexpr double constexpr_fabs(double x) noexcept {
-    return x > 0 ? x : -x;
   }
 
 };
