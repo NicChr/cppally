f34d1f5cc42e44cb8449668cedcd559ed905d267
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-09-12 09:32:51 +0100

    Ensure re-registration when compile = T

--- R/load_all.R
+++ R/load_all.R
@@ -3,6 +3,7 @@
 #' @param path Path to package.
 #' @param debug Should package be built without optimisations?
 #' Default is `FALSE` which builds with optimisations.
+#' @param compile See `?pkgload::load_all`.
 #' @param cppally_header Which header should be
 #' included with the registered C++ code? The default is the full library
 #' "cppally.hpp". Choose "cppally_light.hpp" for the lighter header, which may
@@ -13,8 +14,8 @@
 #' Invisibly registers cppally tagged functions and compiles C++ code.
 #'
 #' @export
-load_all <- function (path = ".", debug = FALSE, cppally_header = c("cppally.hpp", "cppally_light.hpp"), ...){
-  stop_unless_installed(c("pkgload", "rstudioapi"))
+load_all <- function (path = ".", debug = FALSE, cppally_header = c("cppally.hpp", "cppally_light.hpp"), compile = NA, ...){
+  stop_unless_installed(c("pkgload", "pkgbuild", "rstudioapi"))
   if (inherits(path, "package")) {
     path <- path$path
   }
@@ -22,6 +23,13 @@ load_all <- function (path = ".", debug = FALSE, cppally_header = c("cppally.hpp
     rstudioapi::documentSaveAll()
   }
   pkgload::load_all(path = path, debug = debug, compile = FALSE, quiet = TRUE, helpers = FALSE)
+  if (isTRUE(compile)){
+    pkgbuild::clean_dll(path = path)
+  }
   cpp_register(path = path, cppally_header = cppally_header)
-  pkgload::load_all(path = path, debug = debug, ...)
+  if (is.na(compile)){
+    pkgload::load_all(path = path, debug = debug, ...)
+  } else {
+    pkgload::load_all(path = path, debug = debug, compile = compile, ...)
+  }
 }
