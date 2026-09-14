45b8f8626dc05ab256c0a272c24789a40f8e8e2b
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-09-14 09:00:56 +0100

    Reduce visitor instantiations

--- inst/include/cppally/coerce.h
+++ inst/include/cppally/coerce.h
@@ -47,13 +47,17 @@ inline T as_impl(const U& x) {
 // Always visit the SEXP and then convert using the disambiguated type
 template <NotSexp T, AnySexp U>
 inline T as_impl(const U& x) {
-  return internal::visit_sexp(r_sexp(x), []<typename x_t> (const x_t& xvec) -> T {
-    if constexpr (is<x_t, r_sexp>){
-      abort("Don't know how to visit this `r_sexp`");
-    } else {
-      return as<T>(xvec);
-    }
-  });
+  if constexpr (is<U, r_sexp>){
+    return internal::visit_sexp(x, []<typename x_t> (const x_t& xvec) -> T {
+      if constexpr (is<x_t, r_sexp>){
+        abort("Don't know how to visit this `r_sexp`");
+      } else {
+        return as<T>(xvec);
+      }
+    });
+  } else {
+    return as<T>(static_cast<r_sexp>(x));
+  }
 }
 
 // ----- Scalars -----
